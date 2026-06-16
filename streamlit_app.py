f651485c71c199ca79d5442b6261febb40cdf8f6
asmafiza <asmafizax00@gmail.com>
2026-06-17 04:55:22 +0500

    Update histogram to plot 'tenure' instead of 'Age'

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,9 +85,9 @@ plt.title("Correlation Heatmap")
 plt.show()
 # Pehle figure banayein
 fig, ax = plt.subplots()
-st.write("Aapke dataset ke asli columns yeh hain:", df.columns.tolist())
+
 # Phir apna graph banayein (e.g., SNS ya PLT use kar ke)
-sns.histplot(df['Age'], ax=ax)
+sns.histplot(df['tenure'], ax=ax)
 
 # Phir Streamlit ko wo figure pass karein
 st.pyplot(fig)
